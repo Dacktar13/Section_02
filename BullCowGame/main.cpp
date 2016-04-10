b4f48269dcaabf936397fd469a4d5d4dc60974ca
seth <seth@DACKTAR>
2016-04-10 01:59:11 +0100

    Adding header file

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -11,11 +11,13 @@ bool AskToPlayAgain();
 
 // entry point of application
 int main() {
+	bool bPlayAgain = false;
 	do {
 		PrintIntro();
 		PlayGame();
+		bPlayAgain = AskToPlayAgain();
 	}
-	while (AskToPlayAgain());
+	while (bPlayAgain);
 	return 0; // exit application
 }
 
@@ -49,7 +51,7 @@ string GetGuess() {
 }
 
 bool AskToPlayAgain(){
-	cout << "Do you want to play again? ";
+	cout << "Do you want to play again? (y/n) ";
 	string Response = "";
 	getline(cin, Response);
 	return (Response[0] == 'y') || (Response[0] == 'Y');
